fadb84c1a63c5f4a8624d52f348def48f47abc0b
YASHTHECODER <YASHTHECODER@DESKTOP-I23905Q>
2020-01-19 22:43:07 +0530

    just some random change

--- spring-eureka-gallery/src/main/java/com/eureka/gallery/controllers/HomeController.java
+++ spring-eureka-gallery/src/main/java/com/eureka/gallery/controllers/HomeController.java
@@ -27,7 +27,7 @@ public class HomeController {
 	
 	@RequestMapping("/")
 	public String home() {
-		// This is useful for debugging
+		// This is useful for debugging info
 		// When having multiple instance of gallery service running at different ports.
 		// We load balance among them, and display which instance received the request.
 		return "Hello from Gallery Service running at port: " + env.getProperty("local.server.port");
